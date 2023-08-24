87e5173ac93c0c27e8696dd1b469e59c525d3460
Marcu Operario <marcuoperario@gmail.com>
2023-08-24 20:33:43 +0800

    feat: add prevent duplicate transaction name

--- controllers/transaction.controller.ts
+++ controllers/transaction.controller.ts
@@ -2,9 +2,16 @@ import { Request, Response } from 'express';
 import Transaction from '../models/transaction';
 
 export const createTransaction = async (req: Request, res: Response) => {
-    const transaction = new Transaction(req.body);
-
     try {
+        const transactionExists = await Transaction.findOne({
+            transaction_name: req.body.transaction_name
+        });
+
+        if (transactionExists) {
+            throw 'Transaction name already exists!';
+        }
+
+        const transaction = new Transaction(req.body);
         const savedTransaction = await transaction.save();
         res.status(200).json(savedTransaction);
     } catch (error) {
@@ -13,8 +20,8 @@ export const createTransaction = async (req: Request, res: Response) => {
 };
 
 export const updateTransaction = async (req: Request, res: Response) => {
-    const id = req.params.id;
     try {
+        const id = req.params.id;
         const findAndUpdateTransaction = await Transaction.findByIdAndUpdate(id, req.body, { new: true });
         res.status(200).json(findAndUpdateTransaction);
     } catch (error) {
@@ -23,8 +30,8 @@ export const updateTransaction = async (req: Request, res: Response) => {
 };
 
 export const getTransaction = async (req: Request, res: Response) => {
-    const id = req.params.id;
     try {
+        const id = req.params.id;
         const findTransaction = await Transaction.findById(id);
         res.status(200).json(findTransaction);
     } catch (error) {
@@ -57,8 +64,8 @@ export const getTransactions = async (req: Request, res: Response) => {
 };
 
 export const deleteTransaction = async (req: Request, res: Response) => {
-    const id = req.params.id;
     try {
+        const id = req.params.id;
         const findAndDeleteTransaction = await Transaction.findByIdAndDelete(id);
         res.status(200).json(findAndDeleteTransaction);
     } catch (error) {
@@ -68,6 +75,7 @@ export const deleteTransaction = async (req: Request, res: Response) => {
 
 export const deleteTransactions = async (req: Request, res: Response) => {
     try {
+        const id = req.params.id;
         const deleteTransactions = await Transaction.deleteMany();
         res.status(200).json(deleteTransactions);
     } catch (error) {
