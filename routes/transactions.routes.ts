9d41f3d639a18fba3483825e01f6aeef1c03030a
Marcu Operario <marcuoperario@gmail.com>
2023-08-24 20:48:40 +0800

    feat: add route protection

--- routes/transactions.routes.ts
+++ routes/transactions.routes.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import { TRANSACTIONS_BY_ID_ROUTE, TRANSACTIONS_PER_PAGE_ROUTE, TRANSACTIONS_ROUTE } from '../constants/routes';
 import { createTransaction, deleteTransaction, deleteTransactions, getTransaction, getTransactions, getTransactionsPerPage, updateTransaction } from '../controllers/transaction.controller';
+import verifyToken from '../middlewares/verifyToken';
 
 const router = express.Router();
 
-router.get(TRANSACTIONS_PER_PAGE_ROUTE, getTransactionsPerPage);
-router.get(TRANSACTIONS_BY_ID_ROUTE, getTransaction);
-router.get(TRANSACTIONS_ROUTE, getTransactions);
-router.post(TRANSACTIONS_ROUTE, createTransaction);
-router.put(TRANSACTIONS_BY_ID_ROUTE, updateTransaction);
-router.delete(TRANSACTIONS_ROUTE, deleteTransactions);
-router.delete(TRANSACTIONS_BY_ID_ROUTE, deleteTransaction);
+router.get(TRANSACTIONS_PER_PAGE_ROUTE, verifyToken, getTransactionsPerPage);
+router.get(TRANSACTIONS_BY_ID_ROUTE, verifyToken, getTransaction);
+router.get(TRANSACTIONS_ROUTE, verifyToken, getTransactions);
+router.post(TRANSACTIONS_ROUTE, verifyToken, createTransaction);
+router.put(TRANSACTIONS_BY_ID_ROUTE, verifyToken, updateTransaction);
+router.delete(TRANSACTIONS_ROUTE, verifyToken, deleteTransactions);
+router.delete(TRANSACTIONS_BY_ID_ROUTE, verifyToken, deleteTransaction);
 
 export default router;
