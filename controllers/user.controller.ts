b3888907e10e0a6a81a94e1dfe9028a24bb7057e
Marcu Operario <marcuoperario@gmail.com>
2023-08-24 20:37:29 +0800

    refactor: put id variable inside try block

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import User from '../models/user.model';
 
 export const getUser = async (req: Request, res: Response) => {
-    const id = req.params.id;
     try {
+        const id = req.params.id;
         const findUser = await User.find({ _id: id });
         res.status(200).json(findUser);
     } catch (error) {
