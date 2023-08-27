3cd223c6894d7740bd47edc82288b9a6b6143c8f
Marcu Operario <marcuoperario@gmail.com>
2023-08-27 23:39:35 +0800

    feat: update User routes

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { USER_BY_ID_ROUTE } from '../constants/routes';
-import { getUser } from '../controllers/user.controller';
+import { USER_BY_ID_ROUTE, USER_PER_PAGE_ROUTE, USER_ROUTE } from '../constants/routes';
+import { deleteUser, deleteUsers, getUser, getUsers, getUsersPerPage, updateUser } from '../controllers/user.controller';
 import verifyToken from '../middlewares/verifyToken';
 
 const router = express();
 
 router.get(USER_BY_ID_ROUTE, verifyToken, getUser);
+router.get(USER_PER_PAGE_ROUTE, verifyToken, getUsersPerPage);
+router.get(USER_ROUTE, verifyToken, getUsers);
+router.put(USER_BY_ID_ROUTE, verifyToken, updateUser);
+router.delete(USER_BY_ID_ROUTE, verifyToken, deleteUser);
+router.delete(USER_ROUTE, verifyToken, deleteUsers);
 
 export default router;
