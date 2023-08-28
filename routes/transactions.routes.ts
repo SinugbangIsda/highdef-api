a1e0da0d3f806a412caf2d17993b2f233e1b2e36
Marcu Operario <marcuoperario@gmail.com>
2023-08-29 00:16:17 +0800

    feat: add getRecentTransactions  route

--- routes/transactions.routes.ts
+++ routes/transactions.routes.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import { TRANSACTIONS_BY_ID_ROUTE, TRANSACTIONS_PER_PAGE_ROUTE, TRANSACTIONS_ROUTE } from '../constants/routes';
-import { createTransaction, deleteTransaction, deleteTransactions, getTransaction, getTransactions, getTransactionsPerPage, updateTransaction } from '../controllers/transaction.controller';
+import { TRANSACTIONS_BY_ID_ROUTE, TRANSACTIONS_PER_PAGE_ROUTE, TRANSACTIONS_RECENT_ROUTE, TRANSACTIONS_ROUTE } from '../constants/routes';
+import { createTransaction, deleteTransaction, deleteTransactions, getRecentTransactions, getTransaction, getTransactionsPerPage, updateTransaction } from '../controllers/transaction.controller';
 import verifyToken from '../middlewares/verifyToken';
 
 const router = express.Router();
 
+router.get(TRANSACTIONS_RECENT_ROUTE, verifyToken, getRecentTransactions);
 router.get(TRANSACTIONS_PER_PAGE_ROUTE, verifyToken, getTransactionsPerPage);
 router.get(TRANSACTIONS_BY_ID_ROUTE, verifyToken, getTransaction);
-router.get(TRANSACTIONS_ROUTE, verifyToken, getTransactions);
 router.post(TRANSACTIONS_ROUTE, verifyToken, createTransaction);
 router.put(TRANSACTIONS_BY_ID_ROUTE, verifyToken, updateTransaction);
 router.delete(TRANSACTIONS_ROUTE, verifyToken, deleteTransactions);
