8ce754af88ec9f2e149a65012fca32a8c5f50b03
Marcu Operario <marcuoperario@gmail.com>
2023-09-01 02:14:57 +0800

    refactor: update type casing on field

--- models/user.model.ts
+++ models/user.model.ts
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        isActivated: {
+        is_activated: {
             type: Boolean,
             required: true
         }
