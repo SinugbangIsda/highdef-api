ef40d3f6e5f82d327c3af53aac63d2ebae67ebef
Marcu Operario <marcuoperario@gmail.com>
2023-09-06 15:19:32 +0800

    feat: update response limit

--- controllers/transaction.controller.ts
+++ controllers/transaction.controller.ts
@@ -61,7 +61,7 @@ export const getTransactions = async (req: Request, res: Response) => {
 
 export const getRecentTransactions = async (req: Request, res: Response) => {
     try {
-        const transactions = await Transaction.find({ is_deleted: false, status: 'Completed' }).sort({ createdAt: -1 }).limit(10).select('-__v');
+        const transactions = await Transaction.find({ is_deleted: false, status: 'Completed' }).sort({ createdAt: -1 }).limit(5).select('-__v');
         res.status(200).json(transactions);
     } catch (error) {
         res.status(400).json({ message: error });
