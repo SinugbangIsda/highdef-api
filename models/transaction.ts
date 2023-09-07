990c4b142ff950220b55959b533de2a50c8240fb
Marcu Operario <marcuoperario@gmail.com>
2023-09-07 14:14:40 +0800

    feat: revert model timestamp

--- models/transaction.ts
+++ models/transaction.ts
@@ -1,30 +1,31 @@
 import mongoose from 'mongoose';
 
-const transactionSchema = new mongoose.Schema({
-    transaction_name: {
-        type: String,
-        required: true
-    },
-    products: {
-        type: Array<Object>,
-        required: true
-    },
-    is_completed: {
-        type: Boolean,
-        required: true
-    },
-    payment_date: {
-        type: Date,
-        required: false
-    },
-    total_price: {
-        type: Number,
-        required: true
-    },
-    is_deleted: {
-        type: Boolean,
-        required: true
-    }
+const transactionSchema = new mongoose.Schema(
+    {
+        transaction_name: {
+            type: String,
+            required: true
+        },
+        products: {
+            type: Array<Object>,
+            required: true
+        },
+        is_completed: {
+            type: Boolean,
+            required: true
+        },
+        payment_date: {
+            type: Date,
+            required: false
+        },
+        total_price: {
+            type: Number,
+            required: true
+        },
+        is_deleted: {
+            type: Boolean,
+            required: true
+        }
 });
 
 const Transaction = mongoose.model('Transactions', transactionSchema);
