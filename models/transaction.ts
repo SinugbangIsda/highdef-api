cd91759193be4560e9cfa6323573d6dd58702779
Marcu Operario <marcuoperario@gmail.com>
2023-09-07 13:16:13 +0800

    feat: add payment_date field

--- models/transaction.ts
+++ models/transaction.ts
@@ -1,30 +1,31 @@
 import mongoose from 'mongoose';
 
-const transactionSchema = new mongoose.Schema(
-    {
-        transaction_name: {
-            type: String,
-            required: true
-        },
-        products: {
-            type: Array<Object>,
-            required: true
-        },
-        status: {
-            type: String,
-            required: true
-        },
-        total_price: {
-            type: Number,
-            required: true
-        },
-        is_deleted: {
-            type: Boolean,
-            required: true
-        }
+const transactionSchema = new mongoose.Schema({
+    transaction_name: {
+        type: String,
+        required: true
     },
-    { timestamps: true }
-);
+    products: {
+        type: Array<Object>,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    payment_date: {
+        type: Date,
+        required: true
+    },
+    total_price: {
+        type: Number,
+        required: true
+    },
+    is_deleted: {
+        type: Boolean,
+        required: true
+    }
+});
 
 const Transaction = mongoose.model('Transactions', transactionSchema);
 
